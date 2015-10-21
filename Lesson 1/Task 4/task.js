1be33a6a867b88e83c78122163199e3ff3c05f8b
valdman <boris.valdman@live.ru>
2015-10-21 15:40:54 +0300

    Task 4 Есть одна проблема - почему я не вижу подфункцию функции filter из основной программы?

--- Lesson 1/Task 4/task.js
+++ Lesson 1/Task 4/task.js
@@ -1,1 +1,32 @@
-// your code goes here
+var filter = function(arr, criteria){
+    ans = []
+    var inBetween = function(a, b){
+        for(i in arr){
+            if(arr.hasOwnProperty(i)){
+                if(i <= b && i >= a){
+                    ans.push(i)
+                }
+            }
+        }
+    };
+    var isOdd =function(){
+        for(i in arr){
+            if(arr.hasOwnProperty(i)){
+                if(i % 2 == 1){
+                    ans.push(i)
+                }
+            }
+        }
+    };
+    return(criteria)
+};
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+
+var answer = filter(arr, isBetween(3, 6));
+
+for(i in answer){
+    if(answer.hasOwnProperty(i)) {
+        console.log(i);
+    }
+}
