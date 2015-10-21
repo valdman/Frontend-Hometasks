6b09ea7b81cad51a2e3265724475437a15e5360b
valdman <boris.valdman@live.ru>
2015-10-21 14:56:33 +0300

    Task 3 выполнено

--- Lesson 1/Task 3/task.js
+++ Lesson 1/Task 3/task.js
@@ -1,1 +1,27 @@
-// your code goes here
+function byField(field) {
+    return function (a, b) {
+        return a[field] > b[field] ? 1 : -1;
+    };
+}
+
+var users = [{
+    name: "Вася",
+    surname: 'Тестов',
+    age: 20
+}, {
+    name: "Петя",
+    surname: 'Чапаев',
+    age: 25
+}, {
+    name: "Маша",
+    surname: 'Медведева',
+    age: 18
+},
+
+
+];
+
+users.sort(byField("surname"));
+
+users.forEach(function(user){
+    alert(user.name)});
