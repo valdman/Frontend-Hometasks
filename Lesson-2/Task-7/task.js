1ad7053d59c4cdfb223569416882205cfee6c652
valdman <boris.valdman@live.ru>
2015-10-28 17:33:43 +0300

    Lesson 2, Task 7

--- Lesson-2/Task-7/task.js
+++ Lesson-2/Task-7/task.js
@@ -1,1 +1,7 @@
 // your code goes here.
+elements = document.querySelectorAll("div, p, h1");
+console.log(elements);
+console.log(elements.length);
+for(var i = 0; i < elements.length; ++i){
+    elements[i].classList.add("prettify");
+}
